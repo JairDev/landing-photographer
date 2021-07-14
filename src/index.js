8a76c3766ca683b63151ead409208a309aacf0c9
JairDev <devalfredomoscoso@gmail.com>
2021-07-14 13:07:12 -0400

    Mobile Design

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import imagePortfolio4 from "../assets/photo4-port.png";
 import imagePortfolio5 from "../assets/photo5-port.png";
 import imagePortfolio6 from "../assets/photo6-port.png";
 import imagePortfolio7 from "../assets/photo7-port.png";
-import paperCard from "../assets/card-paper.png"
+import paperCard from "../assets/card-paper.png";
 
 const portfolioNode = document.querySelector(".App-portfolio-content-photos");
-const testimonyNode = document.querySelector(".App-testimony-clients")
-const serviceNode = document.querySelector(".App-service-content-services")
+const testimonyNode = document.querySelector(".App-testimony-clients");
+const serviceNode = document.querySelector(".App-service-content-services");
 
 const dataPortfolio = [
   imagePortfolio1,
@@ -19,37 +19,52 @@ const dataPortfolio = [
   imagePortfolio4,
   imagePortfolio5,
   imagePortfolio6,
-  imagePortfolio7
+  imagePortfolio7,
 ];
 
 const dataTestimony = [
-  {"name": "Arquette Duen", "testimony": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s", "src": paperCard},
-  {"name": "Kialsd Sths", "testimony": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s", "src": paperCard},
-  {"name": "Kierñ Lekjr", "testimony": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s", "src": paperCard}
-]
+  {
+    name: "Arquette Duen",
+    testimony:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s",
+    src: paperCard,
+  },
+  {
+    name: "Kialsd Sths",
+    testimony:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s",
+    src: paperCard,
+  },
+  {
+    name: "Kierñ Lekjr",
+    testimony:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s",
+    src: paperCard,
+  },
+];
 
 const dataService = [
   {
-    title: "paquete",
+    title: "Paquete",
     services: {
       dvd: "1 DVD de 30 fotos digitales",
       sesion: "1 hora de sesión fotográfica",
       vestuario: "2 cambios de vestuario",
-      fotos: "3 fotos impresas"
+      fotos: "3 fotos impresas",
     },
-    price: 50
+    price: 50,
   },
   {
-    title: "paquete",
+    title: "Paquete",
     services: {
       dvd: "1 DVD de 30 fotos digitales",
       sesion: "1 hora de sesión fotográfica",
       vestuario: "2 cambios de vestuario",
-      fotos: "3 fotos impresas"
+      fotos: "3 fotos impresas",
     },
-    price: 80
-  }
-]
+    price: 80,
+  },
+];
 
 const fragmentPortfolio = document.createDocumentFragment();
 const fragmentTestimony = document.createDocumentFragment();
@@ -58,46 +73,61 @@ const fragmentService = document.createDocumentFragment();
 dataPortfolio.forEach((photos) => {
   const div = document.createElement("div");
   const img = document.createElement("img");
-  div.setAttribute("class", "portfolio-content-photos")
+  div.setAttribute("class", "portfolio-content-photos");
   img.setAttribute("src", photos);
   img.setAttribute("alt", "Fotografía de una persona");
   div.appendChild(img);
   fragmentPortfolio.appendChild(div);
 });
 
-dataTestimony.forEach(testimony => {
+dataTestimony.forEach((testimony) => {
   const div = document.createElement("div");
   const divContentTestimony = document.createElement("div");
   const testimonyText = document.createElement("p");
   const nameClient = document.createElement("h4");
-  div.setAttribute("class", "testimony")
-  div.appendChild(nameClient)
-  div.appendChild(divContentTestimony)
-  divContentTestimony.setAttribute("class", "testimony-content-testimony")
-  divContentTestimony.appendChild(testimonyText)
-  testimonyText.textContent = testimony.testimony  
-  nameClient.textContent = testimony.name
+  div.setAttribute("class", "testimony");
+  div.appendChild(nameClient);
+  div.appendChild(divContentTestimony);
+  divContentTestimony.setAttribute("class", "testimony-content-testimony");
+  divContentTestimony.appendChild(testimonyText);
+  testimonyText.textContent = testimony.testimony;
+  nameClient.textContent = testimony.name;
   fragmentTestimony.appendChild(div);
 })
 
-dataService.forEach((service) => {
+
+dataService.forEach((service, index) => {
+  let priceFormat = `$${service.price}`;
+  let titleFormat = `${service.title}` + " " + `${index+1}`
   const div = document.createElement("div");
   const divContentTitle = document.createElement("div");
   const ulContentServices = document.createElement("ul");
+  const divContentPrice = document.createElement("div");
+  const divContentButton = document.createElement("div");
+  const button = document.createElement("button");
   const titleService = document.createElement("h4");
+  const price = document.createElement("p");
   Object.values(service.services).forEach((itemService) => {
-    const liServices = document.createElement("li")
-    liServices.textContent = itemService
-    ulContentServices.appendChild(liServices)
-  })
-  div.setAttribute("class", "service")
-  divContentTitle.setAttribute("class", "service-title")
-  div.appendChild(divContentTitle)
-  div.appendChild(ulContentServices)
-  divContentTitle.appendChild(titleService)
-  titleService.textContent = service.title
+    const liServices = document.createElement("li");
+    liServices.textContent = itemService;
+    ulContentServices.appendChild(liServices);
+  });
+  div.setAttribute("class", "service");
+  divContentPrice.setAttribute("class", "content-price");
+  divContentTitle.setAttribute("class", "service-title");
+  divContentButton.setAttribute("class", "content-button-cita");
+  div.appendChild(divContentTitle);
+  div.appendChild(ulContentServices);
+  div.appendChild(divContentPrice);
+  div.appendChild(divContentButton);
+  divContentTitle.appendChild(titleService);
+  divContentButton.appendChild(button);
+  titleService.textContent = titleFormat;
+  price.textContent = priceFormat;
+  button.textContent = "Reserva tu cita";
+  divContentPrice.appendChild(price);
   fragmentService.appendChild(div);
-})
+});
 
 portfolioNode.appendChild(fragmentPortfolio);
 testimonyNode.appendChild(fragmentTestimony);
