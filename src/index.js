289b06785ccebe51d781e629dacbfec407ac108e
JairDev <devalfredomoscoso@gmail.com>
2021-07-21 14:17:27 -0400

    Production test

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import "./styles/main.scss";
 import "./assets/photos-header-portfolio.png";
 import "./assets/name-label.png";
 import "./assets/arrow-scroll.png";
-import "./assets/photos-hero.png";
-// import "./assets/photos-hero.webp";
+// import "./assets/photos-hero.png";
+import "./assets/photos-hero2.png";
 import "./assets/photo-3d-profile.png";
 import "./assets/placeholder-image.jpg";
 import "./assets/link-border-nav.png";
@@ -22,6 +22,11 @@ createTestimonyElements();
 createServiceElements();
 
 let isShowNav = false;
+(function email() {
+  emailjs.init("user_DC5x2D3wdrEpBolyqO9Yr");
+})();
+
+// const formContact
 
 function validate() {
   const formContact = document.getElementById("form-contact");
@@ -44,18 +49,39 @@ function validate() {
       });
   });
 
-  formContact.addEventListener("submit", (e) => {
-    console.log(e);
-    Array.from(input).forEach((value) => {
-      if (!value.value) {
-        errorSubmit.classList.add("show");
-      } else {
-        errorSubmit.classList.remove("show");
-      }
-    });
+  // formContact.addEventListener("submit", (e) => {
+  //   // Array.from(input).forEach((value) => {
+  //   //   if (!value.value) {
+  //   //     errorSubmit.classList.add("show");
+  //   //   } else {
+  //   //     errorSubmit.classList.remove("show");
+  //   //   }
+  //   // });
 
-    e.preventDefault();
-  });
+  //   emailjs.sendForm("service_g", "#form-contact", this).then(
+  //     function () {
+  //       console.log("SUCCESS!");
+  //     },
+  //     function (error) {
+  //       console.log(this)
+  //       console.log("FAILED...", error);
+  //     }
+  //   );
+
+  //   e.preventDefault();
+  // });
+  document.getElementById('form-contact').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    // this.contact_number.value = Math.random() * 100000 | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm('service_g', 'contact_form', formContact)
+        .then(function() {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+});
 }
 
 validate();
@@ -71,8 +97,8 @@ window.addEventListener("load", (e) => {
   const tlHeader = gsap.timeline({
     scrollTrigger: {
       trigger: headerAboutMe,
-      start: "top 60%",
-      end: "10% 30%",
+      start: "top 20%",
+      end: "top top",
       scrub: true,
       // markers: true
     },
@@ -81,8 +107,8 @@ window.addEventListener("load", (e) => {
   const tlPhotoAboutMe = gsap.timeline({
     scrollTrigger: {
       trigger: photoAboutMe,
-      start: "-=170 60%",
-      end: "10% 50%",
+      start: "top 40%",
+      end: "top 10%",
       scrub: true,
       // markers: true
     },
@@ -90,7 +116,7 @@ window.addEventListener("load", (e) => {
   const tlPhotoPortfolio = gsap.timeline({
     scrollTrigger: {
       trigger: photoPortfolio,
-      start: "-=50 10%",
+      start: "-=50 15%",
       end: "10% -=100",
       // scrub: true,
       // markers: true
@@ -104,7 +130,7 @@ window.addEventListener("load", (e) => {
   //animations////////////////////
   if (window.innerWidth > 768) {
     tlPhotoAboutMe.to(photoAboutMe, {
-      rotate: 10,
+      rotate: -20,
       opacity: 1,
       duration: 1,
     });
@@ -122,8 +148,8 @@ window.addEventListener("load", (e) => {
 
   const observer = new IntersectionObserver((entries, observe) => {
     entries.forEach((entrie) => {
-      const defaultSrc = entrie.target.dataset.defaultsrc
-      const src = entrie.target.dataset.src
+      const defaultSrc = entrie.target.dataset.defaultsrc;
+      const src = entrie.target.dataset.src;
       if (entrie.isIntersecting) {
         entrie.target.setAttribute("src", src);
       }
@@ -131,7 +157,6 @@ window.addEventListener("load", (e) => {
   }, options);
   Array.from(hiddenData).forEach((data) => observer.observe(data));
 });
-
 
 document.addEventListener("click", (e) => {
   isShowNav = !isShowNav;
--- webpack.config.js
+++ webpack.config.js
@@ -45,35 +45,35 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "css/style.css",
     }),
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          // ["gifsicle", { interlaced: true }],
-          ["jpegtran", { progressive: true }],
-          ["optipng", { optimizationLevel: 7 }],
-          // Svgo configuration here https://github.com/svg/svgo#configuration
-          // [
-          //   "svgo",
-          //   {
-          //     plugins: extendDefaultPlugins([
-          //       {
-          //         name: "removeViewBox",
-          //         active: false,
-          //       },
-          //       {
-          //         name: "addAttributesToSVGElement",
-          //         params: {
-          //           attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-          //         },
-          //       },
-          //     ]),
-          //   },
-          // ],
-        ],
-      },
-    }),
+    // new ImageMinimizerPlugin({
+    //   minimizerOptions: {
+    //     // Lossless optimization with custom option
+    //     // Feel free to experiment with options for better result for you
+    //     plugins: [
+    //       // ["gifsicle", { interlaced: true }],
+    //       ["jpegtran", { progressive: true }],
+    //       ["optipng", { optimizationLevel: 7 }],
+    //       // Svgo configuration here https://github.com/svg/svgo#configuration
+    //       // [
+    //       //   "svgo",
+    //       //   {
+    //       //     plugins: extendDefaultPlugins([
+    //       //       {
+    //       //         name: "removeViewBox",
+    //       //         active: false,
+    //       //       },
+    //       //       {
+    //       //         name: "addAttributesToSVGElement",
+    //       //         params: {
+    //       //           attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+    //       //         },
+    //       //       },
+    //       //     ]),
+    //       //   },
+    //       // ],
+    //     ],
+    //   },
+    // }),
   ],
 
   module: {
